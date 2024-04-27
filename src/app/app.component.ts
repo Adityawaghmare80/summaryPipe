c7ebab26ea45ff5ffefee3e5e55e6cf50d1aac59
Adityawaghmare80 <148642692+Adityawaghmare80@users.noreply.github.com>
2024-04-27 18:27:21 +0530

    summaryPipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'summaryPipie2';
+  title: string = 'Angular is a TypeScript based, free and open source single page web application framework led by the Angular Team at Google and by a community of individuals and corporations. Angular is a complete rewrite from the same team that built AngularJS.';
 }
